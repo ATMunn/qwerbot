c92fdb19713287833988962b2ecf9de72cedb365
ATMunnGit <iamingodsarmy@gmail.com>
2018-01-13 14:07:07 -0500

    asdf

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 const rls = require("readline-sync");
 
-function askforpassword() {
-    return rls.question("Please enter the password for the bot's account: ", {hideEchoBack: true})
-}
+let pswd = rls.question("Please enter the password for the bot's account: ", {hideEchoBack: true})
+
 
 let config = {
     "port": 6697,
     "host": "irc.freenode.net",
     "nick": "qwerbot",
     "ident": "bot",
-    "password": askforpassword(),
+    "password": pswd,
     "realname": "test bot"
 }
