5f01c65a1cdab9f14098fe089a46ccaae48c79ca
ATMunnGit <iamingodsarmy@gmail.com>
2018-01-21 13:50:58 -0500

    Added permission system

--- commands.js
+++ commands.js
@@ -1,19 +1,53 @@
+const jf = require("jsonfile");
+
 let modul3s = ["general"]; //idk
 
 let commands = {};
 
-function newCommand(name, group, func, help = "No help provided.") {
+function newCommand(name, group, permGroup, func, help = "No help provided.") {
     commands[name] = {
         "group": group,
+        "permGroup": permGroup,
         "function": func,
         "help": help,
     };
 }
 
+function checkPerms(hostmask, command) {
+    let permGroup = commands[command].permGroup;
+    let permissions = jf.readFileSync("./permissions.json");
+    if(permissions.users[hostmask].admin || permissions.groups[permGroup] === "anyone") {
+        return true;
+    }
+    else if(permissions.groups[permGroup] === "restricted") {
+        if (permissions.users[hostmask][permGroup]) {
+            return true;
+        }
+        else {
+            return "noperms";
+        }
+    }
+    else {
+        return "notadmin"
+    }
+}
+
 function parseCommands(bot, msg) {
     for(let cmd in commands) {
         if(msg.bcmd == cmd) {
-            commands[cmd].function(bot, msg);
+            let canExec = checkPerms(msg.hostmask, commands[cmd].permGroup)
+            if(canExec === true) {
+                commands[cmd].function(bot, msg);
+            }
+            else if(canExec === "noperms") {
+                msg.reply("Sorry, you do not have permissions to use the command \""+cmd+"\".")
+            }
+            else if(canExec === "notadmin") {
+                msg.reply("Sorry, the command \""+cmd+"\" is an admin-only command.")
+            }
+            else {
+                msg.reply("You should not see this message. If you do, something has gone wrong...")
+            }
         }
     }
 }
